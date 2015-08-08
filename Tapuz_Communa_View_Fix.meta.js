f7ae96dd0e9ac5e6495835bd17d50d8b3917d2de
Shlomi <shlomisha@users.noreply.github.com>
2015-08-08 15:30:14 +0300

    Create Tapuz_Communa_View_Fix.meta.js

---
+++ Tapuz_Communa_View_Fix.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @id             Tapuz_Communa_View_Fix
+// @name           Tapuz Communa View Fix
+// @version        1.2.0
+// @namespace      shlomisha
+// @author         shlomisha
+// @description    Hides the left and the right panels in Tapuz cummonas, so more space is available for posts
+// @include        http://www.tapuz.co.il/communa/userCommunaMsges.asp?*
+// @run-at         document-end
+// @updateURL      https://raw.githubusercontent.com/shlomisha/Tapuz-Communa-View-Fix/master/Tapuz_Communa_View_Fix.meta.js
+// @downloadURL    https://raw.githubusercontent.com/shlomisha/Tapuz-Communa-View-Fix/master/Tapuz_Communa_View_Fix.user.js
+// ==/UserScript==
